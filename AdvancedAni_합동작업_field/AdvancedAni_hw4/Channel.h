133027381de894f8b87e1105b88692bf7ba82c30
jh5004 <jihun1108@gmail.com>
2019-12-12 19:25:05 +0900

    2 motion

--- AdvancedAni_합동작업_field/AdvancedAni_hw4/Channel.h
+++ AdvancedAni_합동작업_field/AdvancedAni_hw4/Channel.h
@@ -1,15 +1,30 @@
 
 
 #pragma once
+namespace kicker {
+	class Channel {
+		typedef int Index;
+	public:
+		enum ChannelEnum {
+			X_ROTATION, Y_ROTATION, Z_ROTATION,
+			X_POSITION, Y_POSITION, Z_POSITION,
+		};
+		Index          joint;
+		ChannelEnum    type;
+		Index          index;
+	};
+}
 
-class Channel {
-    typedef int Index;
-public:
-    enum ChannelEnum {
-        X_ROTATION, Y_ROTATION, Z_ROTATION,
-        X_POSITION, Y_POSITION, Z_POSITION,
-    };
-    Index          joint;
-    ChannelEnum    type;
-    Index          index;
-};
+namespace keeper {
+	class Channel {
+		typedef int Index;
+	public:
+		enum ChannelEnum {
+			X_ROTATION, Y_ROTATION, Z_ROTATION,
+			X_POSITION, Y_POSITION, Z_POSITION,
+		};
+		Index          joint;
+		ChannelEnum    type;
+		Index          index;
+	};
+}
